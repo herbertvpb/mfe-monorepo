5f71a1ec7ea43a69491c25833644169f8d5c9489
Herbert Vieira <herbert.vieira@clubpetro.com>
2022-08-11 18:21:25 -0300

    implements lazy loading

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 
-import AuthApp from "./components/AuthApp";
-import MarketingApp from "./components/MarketingApp";
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+
 import Header from "./components/Header";
 
 const generateClassName = createGenerateClassName({
@@ -19,10 +20,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
