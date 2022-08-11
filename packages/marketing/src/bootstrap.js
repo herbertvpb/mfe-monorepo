ccb5ff1c8b1ae43999e6455a1c153571e8cfc55c
Herbert Vieira <herbert.vieira@clubpetro.com>
2022-08-11 13:51:13 -0300

    creates communication of routes between the container and marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // Mount function to start up the app
-const mount = (element) => {
-  ReactDOM.render(<App />, element);
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, element);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
+  const defaultHistory = createBrowserHistory();
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory });
   }
 }
 
