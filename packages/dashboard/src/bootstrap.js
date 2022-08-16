e0ab22eb6dd3e6d3f84f9cb29701aff6ff1fb698
Herbert Vieira <herbert.vieira@clubpetro.com>
2022-08-16 17:05:52 -0300

    add initial config to dashboard

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+// Mount function to start up the app
+const mount = (
+  element,
+  { onSignIn, onNavigate, defaultHistory, initialPath }
+) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, element);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+  const defaultHistory = createBrowserHistory();
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory });
+  }
+}
+
+// We are running through container
+// we should export the mount function
+export { mount };
